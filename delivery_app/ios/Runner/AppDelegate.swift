ff63a7e56f4594da09eb594c3e7deaf0671ded9d
saiid27 <saiid27@users.noreply.github.com>
2026-07-05 12:28:33 +0000

    Set iOS build 2 and remove unused location plugins

--- delivery_app/ios/Runner/AppDelegate.swift
+++ delivery_app/ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import Flutter
 import UIKit
-import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -8,7 +7,6 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("YOUR_GOOGLE_MAPS_API_KEY")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
